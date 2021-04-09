bc2bb2251d637557bc9acbd8572786ad2663504b
Aman <54789531+eppisai@users.noreply.github.com>
2021-04-09 10:55:20 +0530

    added 2bit icon enum var in icon template header

--- icon_template.h
+++ icon_template.h
@@ -4,6 +4,6 @@
 
 #include "../../UI/Widgets/Icon2bit.h"
 
-const Icon2bit $ICON_NAME = {$WIDTH, $HEIGHT,{RGB565(255,255,255), RGB565(128,128,128), RGB565(64,64,64), RGB565(0,0,0)}, (const uint8_t[]){$IMAGE_DATA}};
+const Icon2bit $ICON_NAME = {$WIDTH, $HEIGHT, ImageDataFormat::Indexed2Bit, {RGB565(255,255,255), RGB565(128,128,128), RGB565(64,64,64), RGB565(0,0,0)}, (const uint8_t[]){$IMAGE_DATA}};
 
 #endif // $ICON_TEMPLATE_H
